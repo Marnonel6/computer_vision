56962908f47e57ffd08dd1f95ba6dd7401438536
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-04-12 18:33:25 -0500

    Plotted image histogram

--- machine_problems/mp3/mp3.py
+++ machine_problems/mp3/mp3.py
@@ -7,24 +7,19 @@ def main():
     # Load in images
     moon_img = cv2.imread('/home/marno/Classes/Spring23/CV/computer_vision/machine_problems/mp3/test_images/moon.bmp', cv2.IMREAD_GRAYSCALE)
 
-    # Histogram of pixel intensities
-    histogram, bins = np.histogram(moon_img.ravel(), bins=256, range=[0, 256])
 
-    # Plot the histogram
-    plt.plot(histogram, color='black')
-    # Set the axis labels and title
-    plt.xlabel('Pixel Intensity')
-    plt.ylabel('Frequency')
-    plt.title('Histogram of moon image')
-    # Show the plot
-    plt.show()
+
+
+    # Plot histogram of original image
+    image_histogram(moon_img)
+
+
 
     # Display images grid
     moon = cv2.hconcat([np.uint8(moon_img), np.uint8(moon_img)])
 
     # Display
     cv2.imshow(' 1: Normal                                                2: Improved', moon)
-
     # Wait for a key press to close the window
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -37,6 +32,25 @@ args:
     - <arg_name>: (type) <description>
 """
 
+
+"""
+Plot histogram of original image
+
+args:
+    - image: (cv2.IMREAD_GRAYSCALE) Gray scale image
+"""
+def image_histogram(image):
+    # Histogram of pixel intensities
+    histogram, bins = np.histogram(image.ravel(), bins=256, range=[0, 256])
+
+    # Plot the histogram
+    plt.plot(histogram, color='black')
+    # Set the axis labels and title
+    plt.xlabel('Pixel Intensity')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of moon image')
+    # Show the plot
+    plt.show()
 
 
 if __name__ == '__main__':
