de3e02fc26de579ec2497f7104157736fa0342ed
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-04-12 19:41:45 -0500

    Histogram Equalization works

--- machine_problems/mp3/mp3.py
+++ machine_problems/mp3/mp3.py
@@ -7,20 +7,11 @@ def main():
     # Load in images
     moon_img = cv2.imread('/home/marno/Classes/Spring23/CV/computer_vision/machine_problems/mp3/test_images/moon.bmp', cv2.IMREAD_GRAYSCALE)
 
-
-
-
-    # Plot histogram of original image
-    image_histogram(moon_img)
-
     # Preform histogram equalization to improve image quality
-    histogram_equalization(moon_img)
-
-
+    moon_histogram_equalization = histogram_equalization(moon_img)
 
     # Display images grid
-    moon = cv2.hconcat([np.uint8(moon_img), np.uint8(moon_img)])
-
+    moon = cv2.hconcat([np.uint8(moon_img), np.uint8(moon_histogram_equalization)])
     # Display
     cv2.imshow(' 1: Normal                                                2: Improved', moon)
     # Wait for a key press to close the window
@@ -40,6 +31,9 @@ def histogram_equalization(image):
     # Output Gray Level [0,L2]
     L2 = 255
 
+    # Plot histogram of original image intensities
+    image_histogram(image)
+
     # Histogram intensity data from image
     hist, bins = np.histogram(image, bins=256, range=(0, 256))
     # Calculate cumulative distribution
@@ -53,12 +47,22 @@ def histogram_equalization(image):
     plt.xlabel('Input image pixel intensity')
     plt.ylabel('Output image pixel intensity normalized')
     plt.title('Cumulative histogram distribution')
+
+    # Copy image
+    histogram_equalization_image = image.copy()
+    # Get image dimensions
+    height, width = histogram_equalization_image.shape
+    # Preform histogram equalization
+    for u in range(height):
+        for v in range(width):
+            histogram_equalization_image[u,v] = cumulative_normalized[histogram_equalization_image[u,v]]*L2
+    # Plot histogram of new histogram equalization image
+    image_histogram_equalization(histogram_equalization_image)
+
     # Show plots
     plt.show()
 
-    print(f"\n cumulative_hist = {cumulative_normalized}")
-
-
+    return histogram_equalization_image
 
 """
 Plot histogram of original image
@@ -71,14 +75,30 @@ def image_histogram(image):
     histogram, bins = np.histogram(image.ravel(), bins=256, range=[0, 256])
 
     # Plot the histogram
+    plt.figure()
     plt.plot(histogram, color='black')
     # Set the axis labels and title
     plt.xlabel('Pixel Intensity')
     plt.ylabel('Frequency')
-    plt.title('Histogram of moon image')
-    # Show the plot
-    # plt.show()
+    plt.title('Original Histogram of image intensities')
 
+"""
+Plot histogram of new histogram equalization image
+
+args:
+    - image: (cv2.IMREAD_GRAYSCALE) Gray scale image
+"""
+def image_histogram_equalization(image):
+    # Histogram of pixel intensities
+    histogram2, bins2 = np.histogram(image.ravel(), bins=256, range=[0, 256])
+
+    # Plot the histogram
+    plt.figure()
+    plt.plot(histogram2, color='black')
+    # Set the axis labels and title
+    plt.xlabel('Pixel Intensity')
+    plt.ylabel('Frequency')
+    plt.title('Histogram of histogram equalization image intensities')
 
 if __name__ == '__main__':
     main()
