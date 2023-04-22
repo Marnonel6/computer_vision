aa2ce8a84d441c1de2d6e63fab060a1c081884ec
Marno (Mathinus) Nel <marthinusnel2023@u.northwestern.edu>
2023-04-22 14:50:07 -0500

    Gaussian smoothing works

--- machine_problems/mp5/mp5.py
+++ machine_problems/mp5/mp5.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.ndimage import convolve
 
 def main():
 
@@ -14,16 +15,31 @@ def main():
     # Put images next to each other
     test_images = cv2.hconcat([np.uint8(gun1_img), np.uint8(joy1_img), np.uint8(pointer1_img)])
 
-    # Display images grid
-    result_images = cv2.hconcat([np.uint8(GaussSmoothing(gun1_img)), np.uint8(joy1_img), np.uint8(pointer1_img)])
+    # Preform gaussian smoothing
+    smoothed_gun1_img = GaussSmoothing(gun1_img)
+    smoothed_joy1_img = GaussSmoothing(joy1_img)
+    smoothed_pointer1_img = GaussSmoothing(pointer1_img)
+    smoothed_lena_img = GaussSmoothing(lena_img)
+    smoothed_test1_img = GaussSmoothing(test1_img)
+    # Smoothed images grid
+    smoothed_images = cv2.hconcat([np.uint8(smoothed_gun1_img), np.uint8(smoothed_joy1_img), np.uint8(smoothed_pointer1_img)])
+
     # Final images
-    final_images = cv2.vconcat([test_images, result_images])
-    final_images2 = cv2.hconcat([np.uint8(lena_img), np.uint8(GaussSmoothing(lena_img))])
-    final_images3 = cv2.hconcat([np.uint8(test1_img), np.uint8(test1_img)])
+    final_images = cv2.vconcat([test_images, smoothed_images])
+    final_images2 = cv2.hconcat([np.uint8(lena_img), np.uint8(smoothed_lena_img)])
+    final_images3 = cv2.hconcat([np.uint8(test1_img), np.uint8(smoothed_test1_img)])
     # Display
     cv2.imshow('Canny edge detection', final_images)
     cv2.imshow('Canny edge detection2', final_images2)
     cv2.imshow('Canny edge detection3', final_images3)
+
+    # test numpy array
+    a = np.array([[38, 66, 65], [14, 35, 64], [12, 15, 42]])
+    magnitude, direction = ImageGradient(a)
+    print(magnitude)
+    print(direction)
+    # cv2.imshow('magnitude', magnitude)
+    # cv2.imshow('direction', direction)
     # Wait for a key press to close the window
     cv2.waitKey(0)
     cv2.destroyAllWindows()
@@ -33,8 +49,10 @@ Gaussian smoothing
 
 args:
     - image: (cv2 - BGR image) image to apply gaussian smoothing to
+    - N: (int) size of the gaussian kernel [NxN]
+    - sigma: (float) standard deviation of the gaussian distribution
 return:
-    - gs_smoothed_image: (cv2 - BGR image) gaussian smoothed image
+    - smoothed: (cv2 - BGR image) gaussian smoothed image
 """
 def GaussSmoothing(image, N=5, sigma=1):
     # Define the Gaussian filter kernel
@@ -62,6 +80,50 @@ def GaussSmoothing(image, N=5, sigma=1):
 
     return smoothed
 
+"""
+Image gradient [Magnitude, Direction]
+
+args:
+    - image: (cv2 - BGR image) image to apply image gradient to
+return:
+    - magnitude: (cv2 - BGR image) magnitude of the image gradient
+    - direction: (cv2 - BGR image) direction of the image gradient
+"""
+def ImageGradient(image):
+    # Convert to grayscale
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = image
+
+    # Sobel operators
+    Gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
+    Gy = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
+
+    # print Gx and Gy
+    print(Gx)
+    print(Gy)
+
+    # Apply Sobel convolution kernels
+    Ix = convolve(gray, Gx)
+    Iy = convolve(gray, Gy)
+
+    # Compute magnitude and direction
+    magnitude = np.sqrt(Ix*Ix + Iy*Iy)
+    direction = 180 + np.arctan2(Iy, Ix)*(180/np.pi) # In degrees
+
+
+    # # Convert to grayscale # NOTE OpenCV implimentation
+    # gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # # Apply sobel operator
+    # sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=5)
+    # sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=5)
+    # # Compute magnitude and direction
+    # magnitude = np.sqrt(sobelx*sobelx + sobely*sobely)
+    # direction = np.arctan2(sobely, sobelx)
+    # # Convert to BGR
+    # magnitude = cv2.cvtColor(np.uint8(magnitude), cv2.COLOR_GRAY2BGR)
+    # direction = cv2.cvtColor(np.uint8(direction), cv2.COLOR_GRAY2BGR)
+
+    return magnitude, direction
 
 
 if __name__ == '__main__':
